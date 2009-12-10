d3c887b802d4758275951f1b46a812cb5c29e722
Gergely Imreh <imrehg@gmail.com>
2009-12-10 18:35:48 +0800

    PEP8 format cleanup

--- twophoton/cascade.py
+++ twophoton/cascade.py
@@ -2,7 +2,7 @@
 # Cascade configuration two photon absorption calc
 # Based on: DOI: 10.1103/PhysRevLett.100.203001
 
-from numpy import * 
+from numpy import *
 from pylab import *
 from time import time
 
@@ -26,7 +26,7 @@ wlevels = c/wavellevels*2*pi
 # Center frequency:
 fcentre = sum(wlevels)/2 / (2*pi)
 # 2-level freqency in angular units: centre locked on it
-wtwo = 2 * (2 * pi * fcentre) 
+wtwo = 2 * (2 * pi * fcentre)
 ########################
 
 twopifcentre = 2 * pi * fcentre
@@ -35,18 +35,21 @@ offs = wlevels[0] - twopifcentre
 offs2 = wtwo - fourpifcentre
 decay = decayrate[0]
 
+
 def cmode(params, x):
     (wtwo, fcentre, freqrep, decayrate, (m, n)) = params
     return 1 / (1j * (offs - x * 2 * pi * freqrep) + decay)
+
 
 def cgf(params):
     (wtwo, fcentre, freqrep, decayrate, (m, n)) = params
     return 1 / (1j * (offs2 - (m + n) * 2 * pi * freqrep) +
         decayrate[1]) * (cmode(params, n) + cmode(params, m))
 
+
 def gettrans(params):
     (wtwo, fcentre, freqrep, decayrate, (m, n)) = params
-    modesn  = floor((wlevels/(2*pi) - fcentre) / freqrep)
+    modesn = floor((wlevels/(2*pi) - fcentre) / freqrep)
     goodmode = abs(int(modesn[0]))
     ctot = 0j
     nterms = 11
@@ -55,6 +58,7 @@ def gettrans(params):
             params = (wtwo, fcentre, freqrep, decayrate, (-m, n))
             ctot += cgf(params)
     return ctot
+
 
 def scan(freqrepscan):
     trans = []
@@ -99,7 +103,7 @@ res = res + array(scan(freqscan))
 elapsed = time() - start
 print "Elapsed time:", elapsed
 
-final = abs(res) 
+final = abs(res)
 
 semilogy((freqscan-90e6)/1e3, final, '.-')
 xlabel('Repetition frequency - 90 Mhz (kHz)')
