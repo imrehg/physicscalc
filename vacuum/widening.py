ea6be6c308c38478a298704c3a2c7b309e53cff4
Gergely Imreh <imrehg@gmail.com>
2010-02-04 22:54:06 +0800

    vacuum: sum calculation enhancement
     + put things in functions
     + can handle arbitrary tube shapes with only small code change
     + plot a series of calculation

--- vacuum/widening.py
+++ vacuum/widening.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import division
-from scipy import linspace, diff
+from scipy import *
+from pylab import *
 
 # Vacuum system calculation for non uniform tube thickness
 # Based on Building Scientific Apparatus, p96-97
@@ -9,16 +10,14 @@ from scipy import linspace, diff
 v = 2e4
 
 # Length of tube (cm)
-L = 5
+L = 2
 
 # Diameter (cm), start, finish, dD/dL
-D0 = 0.5
-Dt = 4
-k = (Dt - D0) / L
+D0 = 1
+k = 2
 
 # number of tube subdivisions
-n = 10000
-
+n = 400
 
 def conduct(D, l, v):
     """ Vacuum conductance
@@ -34,17 +33,70 @@ def conduct(D, l, v):
     return 2.6e-4 * v * D ** 3 / l
 
 
-# Subdivide the tube into smaller segments
-l = linspace(0, L, n+1)
-dl = diff(l)
+def diameter(l, params):
+    """ Tube diameter
+    Standard is linear change, but can add arbitrary functions
+    """
+    # Linear
+    (k, D0) = params
+    D = k * l + D0
 
-# Connect these segments into series
-# Cout = 1 / (Sum(1 / Ci))
-Cout = 0
-for i in range(0, n):
-    li = l[i] + dl[i]/2
-    D = k * li + D0
-    Cout += 1.0 / conduct(D, dl[i], v)
-Cout = 1.0/Cout
+    # Sine
+    #(k, D0) = params
+    #D = D0 + 1/k*sin(l)
+    
+    return D
 
-print "Total conductance with %d tube segments: %.3f L/s" %(n, Cout)
+def sumconduct(L, n, D0, k):
+    """ Total conductance of the vacuum tube
+
+    Input:
+     L : total length
+     n : number of segments
+     D0, k : tube geometry parameters, depending on diameter(),
+             for the linear these are the starting diameter and 
+             change/unit length, respectively
+
+    Output:
+     Cout: total conductance
+    """
+    # Subdivide the tube into smaller segments
+    l = linspace(0, L, n+1)
+    dl = diff(l)
+
+    # Connect these segments into series
+    # Cout = 1 / (Sum(1 / Ci))
+    Cout = 0
+    for i in range(0, n):
+        li = l[i] + dl[i]/2
+        params = (k, D0)
+        D = diameter(li, params)
+        Cout += 1.0 / conduct(D, dl[i], v)
+    Cout = 1.0/Cout
+    return Cout
+
+####################################
+
+# Series calculation along a whole tube as a function of its length
+mn = 30
+Llist = linspace(1,10,mn)
+Con = zeros(mn)
+DD = zeros(mn)
+for m in range(0,mn):
+    Con[m] = sumconduct(Llist[m], n, D0, k)
+    #params = (k, D0)
+    #DD[m] = diameter(Llist[m], params)
+
+
+# Plot results
+figure()
+title('Sums')
+plot(Llist, Con)
+xlabel('Distance (cm)')
+ylabel('Vacuum conductance (L/s)')
+
+#figure()
+#plot(Llist, DD)
+#ylabel('Diameter')
+
+show()
