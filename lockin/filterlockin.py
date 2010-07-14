f23897c18d67a2dcabbb5733cf63d0df09302e84
Gergely Imreh <imrehg@gmail.com>
2010-07-14 14:08:13 +0800

    lockin: add different model (closer to CPT) and a bit more organization

--- lockin/filterlockin.py
+++ lockin/filterlockin.py
@@ -6,11 +6,18 @@ from pylab import plot, show, legend, figure, title, xlabel, ylabel
 ## Input parameters
 # filter frequency, in modulation frequency units
 fc = 1
+# Background levels: [input-independent, input-dependent]
+bglevel = [0.1, 1]
 # lock-in time constant
 Ts = 6
+# Signal model:
+# 0: original low-pass
+# 1: slow build up, quick decay, (similar to CPT)
+model = 1
 
 # Helper functions
 ph2deg = lambda ph: ph/pi*180
+chop = lambda t, a, b : a if mod(ceil(t*2),2) > 0 else b
 
 # Filter response calculation
 def filterresponse(t, fc):
@@ -34,10 +41,13 @@ dt = 0.025
 t = array(arange(0,20,dt))
 # linspace(0, 10, 400)
 ft = filterresponse(t, fc)
-bggen = lambda t, bglevel: bglevel[0]+bglevel[1] if mod(ceil(t*2),2) > 0 else bglevel[0]
-bglevel = [0.1, 1]
-bg = array([bggen(tt, bglevel) for tt in t])
-signal1 = ft + bg
+bg = array([chop(tt, bglevel[1]+bglevel[0], bglevel[0]) for tt in t])
+
+if model == 1:
+    for n,ti in enumerate(t):
+        ft[n] = chop(ti, -ft[n], 0)
+
+signal1 = bg + ft
 signal2 = bg
 figure()
 plot(t, signal1, label="Signal+bg")
