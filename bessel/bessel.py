aecdad5b6bb02696f4a42e62909cb6f5cabfe971
Gergely Imreh <imrehg@gmail.com>
2011-03-22 16:51:32 +0800

    update bessel function calculation with additional data

--- bessel/bessel.py
+++ bessel/bessel.py
@@ -3,11 +3,16 @@ import pylab as pl
 import scipy as sp
 from scipy.special import jv
 
-maxorder = 2
+maxorder = 3
 moddepth = np.linspace(0, 10, 100001)
 
-markers = ['-',':','--']
-name = ['0th', '1st', '2nd']
+pl.figure(num=None, figsize=(8.27, 11.69), dpi=120, facecolor='w', edgecolor='k')
+
+### Comparison of orders
+pl.subplot(2,1,1)
+
+markers = ['-',':','--','-.']
+name = ['0th', '1st', '2nd', '3rd', '4th']
 
 olist = []
 for order in xrange(0, maxorder+1):
@@ -23,11 +28,30 @@ for o1 in xrange(0, maxorder):
         crossv = moddepth[zval]
         print "=== Equal amplitude for orders %d - %d ===" %(o1, o2)
         print crossv
-        text += "Orders: %d -> %d\n%s\n\n" %(o1, o2, crossv)
+        text += "Orders: %d -> %d\n%s\n" %(o1, o2, crossv)
         pl.plot(crossv, olist[o1][zval], '.', markersize=10)
 
-pl.text(3, 0.3, text, size=12)
+pl.text(1.5, 0.35, text, size=12)
 pl.xlabel('Modulation depth')
 pl.ylabel('Amplitude (normalized)')
 pl.legend(loc="upper right")
+
+### Total
+pl.subplot(2,1,2)
+for i in xrange(len(olist)):
+    olist[i] *= (-1)**i
+total = sum(olist)**2
+tdiff = np.diff(total)
+mins = sp.where(tdiff[:-1] * tdiff[1:] < 0)
+mstep = moddepth[1] - moddepth[0]
+pl.plot(moddepth, total)
+pl.plot(moddepth[mins][0::2]+mstep/2, np.zeros(len(mins[0][0::2])), '.', markersize=10)
+text = "Min: %s\nMax: %s\nMaxval: %s" %(moddepth[mins][0::2], moddepth[mins][1::2], total[mins][1::2])
+pl.text(1.2, 0.3, text, size=12)
+pl.xlabel('Modulation depth')
+pl.ylabel('Predicted interraction')
+pl.title('Up to order %d' %(maxorder))
+print "== Total effect: extremas =="
+print moddepth[mins]+mstep/2
+
 pl.show()
