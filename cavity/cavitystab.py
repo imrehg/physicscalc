6cb0c286516c6782db57a1ad620501600021d988
Gergely Imreh <imrehg@gmail.com>
2011-04-13 20:18:21 +0800

    more work on cavitystab, but it's buggy at the moment

--- cavity/cavitystab.py
+++ cavity/cavitystab.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 from __future__ import division
 import numpy as np
 import pylab as pl
@@ -32,7 +32,6 @@ def getwidths(L, MR, q, lam, n):
     res = cavity(q, mirror, n)
     widths = np.array([np.sqrt(-1/(np.imag(1/r))*lam/np.pi) for r in res])
     return widths
-
 
 def basiccompare():
     l = 852e-9
@@ -68,5 +67,83 @@ def basiccompare():
     pl.legend(loc='best')
     pl.show()
 
+
+def modematch():
+    l = 852e-9
+    w0 = 0.364e-3
+    n = 20
+
+
+    pl.figure(figsize=(11.69, 8.27), dpi=100)
+
+    zlist = np.linspace(-1, 1, 51)
+    minz = 0
+    minval = 100
+    for z in zlist:
+        L = 0.8
+        MR = 1.0
+        w, R, q = gbeam(z, l, w0)
+        name = "z = %g" %(z)
+        widths = getwidths(L, MR, q, l, n)/w0
+        pl.plot(range(1,n+1), widths, '.-', label=name)
+        wrange = max(widths)-min(widths)
+        if wrange < minval:
+            minval = wrange
+            minz = z
+    print "Best: %g (%g)" %(minz, minval)
+
+    pl.xlabel('Round trip number')
+    pl.ylabel('Beam waist at output (compared to input)')
+    pl.xlim([0, n])
+
+    # pl.legend(loc='best')
+    pl.show()
+
+def waistsize(R1, R2, d, lam):
+    return ((lam / np.pi)**2 * (d * (R1 - d) * (R2 - d) * (R1 + R2 - d)) / (R1 + R2 - 2 * d)**2)**(0.25)
+
+def wfromq(q, lam):
+    temp = 1/q
+    R = 1/np.real(temp)
+    w = np.sqrt(-lam / (np.pi * np.imag(temp)))
+    return (w, R)
+
+def qfromw(w0, z, lam):
+    zR = np.pi * w0**2 / lam
+    R = z * (1 + (zR / z)**2)
+    w = w0 * np.sqrt(1 + (z / zR)**2)
+    q = 1 / ((1 / R) - (1j * lam / (np.pi * w**2)))
+    return q
+
+
+
+def findlens(R, d, lam, w0):
+    w0c =  waistsize(R, R, L, lam)
+    zc = d/2
+    zR = np.pi * w0c**2 / lam
+    wR = zc * (1 + (zR / zc)**2)
+    w = w0 * np.sqrt(1 + (zc / zR)**2)
+    q = 1 / ((1 / wR) - (1j * lam / (np.pi * w**2)))
+    q2 = (1 * q + 0) / (0.51 * q + 1)
+    
+    wout, Rout = wfromq(q2, lam)
+    return (wout, Rout)
+
+    # return w0c, zR, wR, w, q, q2
+
+# def lenseffect(d, z, f, w0, lam)
+
+
+
 if __name__ == "__main__":
-    basiccompare()
+    # basiccompare()
+    # modematch()
+    R = 1
+    L = 0.8
+    lam = 852e-9
+    # print waistsize(R, R, L, 852e-9) * 1e6
+    # print (1 - L/R)*(1 - L/R)
+
+    print findlens(R, L, lam, 0.5e-3)
+    q = qfromw(5e-4, 1, lam)
+    print wfromq(q, lam)
