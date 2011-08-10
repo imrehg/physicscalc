1fc41c64a4aff0a20311a701c10ac34d9472ee99
Gergely Imreh <imrehg@gmail.com>
2011-08-10 12:51:12 +0800

    zeemanslower: level calculation

--- zeemandesign/fieldcalc.py
+++ zeemandesign/fieldcalc.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import numpy as np
+import pylab as pl
 
 hbar = 1.05457148e-34
 uB = hbar * 2 * np.pi * 1.399e6
@@ -25,4 +26,45 @@ tU = 1/GRb # Time: 1/linewidth
 xU = 1/kRb # Distance: 1/wavenumber
 
 # Dimensionless detuning
-print detu * tU
+# print detu * tU
+
+v0 = 365
+k = 1281655*2*np.pi
+
+# dd = -2*np.pi*265e6 + k*v0
+# print dd/ 2/ np.pi / 1e6
+
+
+# print hbar*k/uprime*v0
+
+a = -v0
+t = np.linspace(0, 1, 101)
+vt = v0 + a*t
+xt = v0*t + a*t**2/2
+dt = (-2*np.pi*260e6 + k*vt)/2/np.pi/1e6
+
+b0 = -285
+dd = (b0 + hbar*k/uprime*np.sqrt(v0**2 + 2*a*xt))*uprime/hbar/2/np.pi/1e6
+print dd
+dfield = dt/uprime*hbar*2*np.pi*1e6
+pl.plot(xt, dfield)
+# pl.plot(0, detu/2/np.pi/1e6, 'x')
+# pl.plot(xt, dd, '.-')
+# pl.plot(xt, v0**2 + 2*a*xt)
+print dfield[-1]
+print dfield[-1]/-150
+a1 = (gP3*me - gS1*mg)
+a2 = (gP3*4 - gS1*3)
+print a1, a2, a1/a2, a2/a1
+# pl.show()
+
+print "field:", 2*np.pi*260e6/uprime*hbar
+
+
+def gf(gi, gj, i, j, f):
+    return gj*(f*(f+1) - i*(i+1) + j*(j+1))/(2*f*(f+1)) + gi*(f*(f+1) + i*(i+1) - j*(j+1))/(2*f*(f+1))
+
+# P3/2
+ge = gf(-0.0003, 1.3362, 5/2, 3/2, 4)
+gg = gf(-0.0003, 2.002, 5/2, 1/2, 3)
+print ge*4 - gg*3
