752794f499eb072acb07a28280813c4452132416
Gergely Imreh <imrehg@gmail.com>
2011-10-24 17:01:32 +0800

    zeeman: recent updates

--- zeemandesign/cesiumrepeat.py
+++ zeemandesign/cesiumrepeat.py
@@ -36,24 +36,37 @@ def CsPv(T):
 def doublePinFrac(r, L):
     return (L**2 + 2*r**2 - L*np.sqrt(L**2 + 4*r**2))/(2*r**2)
 
+def partialPinhole(ymax, r, L):
+    X = lambda y: 2/np.pi*(np.arccos(y) - y*np.sqrt(1-y**2))*2*y*(2*r*L / (L**2+4*r**2*y**2))**2
+    return integ.quad(X, 0, ymax)[0]
+
+def ttime(v, vf, l1, l2, l3, a):
+    lB = (v**2 - vf**2)/(2*a)
+    t1 = l1/v
+    t2 = (l2 - lB)/v
+    t3 = (v - np.sqrt(v**2 - 2 * a * lB))/(2*a)
+    t4 = l3 /vf
+    return t1+t2+t3+t4
+
 if __name__ == "__main__":
 
     #### All parameters
     atom = zs.Cs133()
-    T = 60 + 273 # Kelvin
+    T = 80 + 273 # Kelvin
     P = 3e-5 # vapour pressure in torr
     r = 1e-3 # pinhole radius
     lcoll = 76.2e-3 # collimator length
-    # eta = 0.5
-    # v0 = 154
-    # vf = 42
-    eta = 0.82
-    v0 = 205
-    vf = 50
+    eta = 0.5
+    v0 = 154
+    vf = 42
+    # eta = 0.82
+    # v0 = 205
+    # vf = 50
     r2 = 3.5e-3
     l1 = 200e-3
     l2 = 400e-3
     l3 = 150e-3
+    rmot = (25.4e-3)/2
     #### No parameters after this
     P = CsPv(T) # fix vapour pressure from doc
     print "Calculated vapour pressure %g torr" %(P)
@@ -68,6 +81,7 @@ if __name__ == "__main__":
 
     influx = r**2 * np.pi * baseflux
     print "Flux through first pinhole: ", influx
+
 
     dPin = doublePinFrac(r, lcoll)
     print "Fractional flux out of double pinhole:", dPin
@@ -113,5 +127,45 @@ if __name__ == "__main__":
         print("Revised flux --> %g" %(influx4))
     else:
         influx4 = influx3
+
+    # print "Transit time for", v0, " m/s:", ttime(v0, vf, l1, l2, l3, atom.aslow*eta)
+    pl.figure()
+    vz = np.linspace(vf, v0, 101)
+
+    # pl.plot(v, rmot/(v*ttime(v, vf, l1, l2, l3, atom.aslow*eta)))
+    # pl.plot([v[0], v[-1]], [thmax, thmax])
+    # pl.plot([v[0], v[-1]], [thmaxz, thmaxz])
+    tr = ttime(vz, vf, l1, l2, l3, atom.aslow*eta)
+
+    tr2 = ttime(vz, vf, l1, l2, 0, atom.aslow*eta)
+
+    pl.plot(vz, vz*np.tan(2*r/lcoll), 'k-', label='collimator limited', linewidth=2)
+    pl.plot(vz, rmot/tr, 'r--', label='transverse broading limited (at MOT)', linewidth=2)
+    pl.plot(vz, r2/tr2, 'b:', label='transverse broading limited (at slower)', linewidth=2)
+    pl.xlabel('Vz (m/s)')
+    pl.ylabel('Vr (m/s)')
+    pl.legend(loc='best')
+
+
+    # dblquad(lambda t, x: exp(-x*t)/t**n, 0, Inf, lambda x: 1, lambda x: Inf)
+    def intfunc(vr, vz):
+        theta = np.arctan(vr / vz)
+        y = lcoll * (vr / vz) / (2 * r)
+        return vpdf(np.sqrt(vr**2 + vz**2))/vnorm*np.cos(theta)*np.sin(theta)*2/np.pi*(np.arccos(y) - y*np.sqrt(1-y**2))
+
+    # translim = integ.dblquad(intfunc, vf, v0, lambda x: 0, lambda x: rmot/ttime(x, vf, l1, l2, l3, atom.aslow*eta))[0]
+    translim = integ.dblquad(intfunc, vf, v0, lambda x: 0, lambda x: r/ttime(x, vf, l1, l2, 0, atom.aslow*eta))[0]
+    transtot = integ.dblquad(intfunc, vf, v0, lambda x: 0, lambda x: np.tan(thmax)*x)[0]
+    # # Debug plotting
+    # vx = v0
+    # vr = np.linspace(0, np.tan(thmax)*vx, 101)
+    # vr2 = np.linspace(0, r2/ttime(vx, vf, l1, l2, 0, atom.aslow*eta), 101)
+    # pl.figure()
+    # pl.plot(vr, intfunc(vr, vx))
+    # pl.fill_between(vr2, intfunc(vr2, vx))
+    transfraq = translim/transtot
+    influx4 = vfraq * dPin * transfraq * influx
+    print("Revised flux2--> %g" %(influx4))
+
 
     pl.show()
