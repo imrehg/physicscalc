d96ff62d4bbfae19655138af0c245d5bd60a6f3a
Gergely Imreh <imrehg@gmail.com>
2011-12-02 14:45:06 +0800

    zeeman: layeroptimize updates

--- zeemandesign/layeroptimize.py
+++ zeemandesign/layeroptimize.py
@@ -192,54 +192,59 @@ if __name__ == "__main__":
     import wires
     import zeemanslower as zs
 
-
-    R = 0.0383
-    eta, v0, vf, detu = 0.7, 365, 20, 260
+    # Parameters
+    R = 0.30 / 2 # larger diameter slower tube
+    eta = 0.5 # efficiency
+    Ls = 0.6 # set slower length
+    vf = 30
     # The field that we want to match
     nz = 61
     atom = zs.Rb85()
     sl = zs.slowerlength(atom.aslow, eta, v0, vf)
     z = np.append([-5.5*R], np.append(np.linspace(0, sl, nz-2), sl+5.5*R))
-    bfield = zs.bideal(atom, z, eta, v0, vf, detu)
+    v0 = np.sqrt(2 * l2 * atom.aslow * eta + vf**2)
 
 
-    wire = wires.AWG9
-    # # Our coil
-    nloops, loops, layer, csign, segments = createStruct((z[0],z[-1]), wire[0], (10, 10))
+    # bfield = zs.bideal(atom, z, eta, v0, vf, detu)
 
-    setup = {'looppos': loops,
-             'layer': layer,
-             'csign': csign,
-             'segments': segments,
-             'R': R,
-             'd': wire[0],
-             }
+
+    # wire = wires.AWG9
+    # # # Our coil
+    # nloops, loops, layer, csign, segments = createStruct((z[0],z[-1]), wire[0], (10, 10))
+
+    # setup = {'looppos': loops,
+    #          'layer': layer,
+    #          'csign': csign,
+    #          'segments': segments,
+    #          'R': R,
+    #          'd': wire[0],
+    #          }
     
-    newsetup = optimize(z, bfield, setup, maxtry=10000)
-    # newsetup = setup
-    # pl.plot(z, bfield, 'x')
-    ze = np.linspace(z[0], z[-1], 201)
-    pl.figure(figsize=(11.69, 8.27))
-    pl.plot(ze, fieldcalc(ze, newsetup)/fieldcalc(0, newsetup), 'r-', linewidth=2, label='coil field')
-    pl.plot(z, bfield/bfield[1], 'ko', markersize=5, label='target field')
-    pl.title("%s, R: %g mm, v: %d-%d m/s, %d MHz" %(wire[2], R*1e3, v0, vf, detu))
-    pl.xlabel('position (m)')
-    pl.ylabel('normalized magnetic field')
-    pl.legend(loc='best')
-    # pl.plot(zz, normalize(fieldcalc(zz, newsetup), 10), 'r-')
-    # print newsetup['segments']
+    # newsetup = optimize(z, bfield, setup, maxtry=10000)
+    # # newsetup = setup
+    # # pl.plot(z, bfield, 'x')
+    # ze = np.linspace(z[0], z[-1], 201)
+    # pl.figure(figsize=(11.69, 8.27))
+    # pl.plot(ze, fieldcalc(ze, newsetup)/fieldcalc(0, newsetup), 'r-', linewidth=2, label='coil field')
+    # pl.plot(z, bfield/bfield[1], 'ko', markersize=5, label='target field')
+    # pl.title("%s, R: %g mm, v: %d-%d m/s, %d MHz" %(wire[2], R*1e3, v0, vf, detu))
+    # pl.xlabel('position (m)')
+    # pl.ylabel('normalized magnetic field')
+    # pl.legend(loc='best')
+    # # pl.plot(zz, normalize(fieldcalc(zz, newsetup), 10), 'r-')
+    # # print newsetup['segments']
 
-    simulation = {'R': R,
-                  'eta': eta,
-                  'v0': v0,
-                  'vf': vf,
-                  'detu': detu,
-                  'wire': wire,
-                  'setup': newsetup
-                  }
-    series = 0
-    savefile = "%d_%s" %(series, wire[2])
-    np.savez("%s", simulation=simulation)
-    pl.savefig('%s.png' %savefile)
-    pl.show()
+    # simulation = {'R': R,
+    #               'eta': eta,
+    #               'v0': v0,
+    #               'vf': vf,
+    #               'detu': detu,
+    #               'wire': wire,
+    #               'setup': newsetup
+    #               }
+    # series = 0
+    # savefile = "%d_%s" %(series, wire[2])
+    # np.savez("%s", simulation=simulation)
+    # pl.savefig('%s.png' %savefile)
+    # pl.show()
     
