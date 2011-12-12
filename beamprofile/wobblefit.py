51a37b588138354c2da839f80aa0c6efe46cd94b
Gergely Imreh <imrehg@gmail.com>
2011-12-12 15:33:15 +0800

    wobblefit: more work on it

--- beamprofile/wobblefit.py
+++ beamprofile/wobblefit.py
@@ -7,13 +7,13 @@ def gauss2d(cx, cy, sx, sy, h, X, Y):
     res = h * np.exp(-(X - cx)**2 / (2*sx**2)) * np.exp(-(Y - cy)**2 / (2*sy**2))
     return res
 
-def waves(th, period, phase, h, offset, X, Y):
+def waves(th, period, phase, amp, X, Y):
     pos = np.cos(th)*X + np.sin(th)*Y
-    ret = np.sin(2*np.pi*pos/period + phase) * h + offset 
+    ret = np.sin(2*np.pi*pos/period + phase) * amp
     return ret
 
 def wobble(cx, cy, sx, sy, h, th, period, phase, amp, offset):
-    return lambda X, Y: gauss2d(cx, cy, sx, sy, h, X, Y) * waves(th, period, phase, amp, offset, X, Y)
+    return lambda X, Y: gauss2d(cx, cy, sx, sy, h, X, Y) * (1 + waves(th, period, phase, amp, X, Y)) + offset
 
 def fitwobble(data, X, Y, p0):
     """Returns (height, x, y, width_x, width_y)
@@ -31,10 +31,10 @@ if __name__ == "__main__":
     Y = Y.T
 
     th = -56/180*np.pi
-    period = 40
-    phase = 0.5
+    period = 36.5
+    phase = 0.2
     amp = 0.2
-    offset = 0.8
+    offset = data[0, 0]
     cx = 314
     cy = 260
     sx = 100
@@ -42,13 +42,17 @@ if __name__ == "__main__":
     h = 175
 
     p0 = (cx, cy, sx, sy, h, th, period, phase, amp, offset)
-    # fitfun = np.ravel(wobble(*p)(X, Y) - data)
-    # print fitfun
+    # # fitfun = np.ravel(wobble(*p)(X, Y) - data)
+    # # print fitfun
     p = fitwobble(data, X, Y, p0)
-    print p
+    # print p
+  #   p = [ 3.22891320e+02,   2.59297580e+02,   1.40109060e+02,   1.40215415e+02,
+  #  1.42835103e+02,  -1.05651955e+00,   3.65765445e+01,   1.36331418e+00,
+  # -1.22638586e-01,   8.96411783e-01,]
+    # p = p0
     vals = wobble(*p)(X, Y)
-    cx, cy, sx, sy, h, th, period, phase, amp, offset = p
-
+    cx, cy, sx, sy, h, th, period, phase, amp, offset, = p
+    print th, period, phase, amp
 
     fh, fw = 8.27, 11.6
     # Plotting
@@ -88,5 +92,22 @@ if __name__ == "__main__":
     centre.axes.set_ylim([dimy-1, 0])
 
     print np.sqrt(np.sum((data - vals)**2))
+
+    # pl.figure()
+    # pl.subplot(211)
+    # xstep = np.array(range(0, 640))
+    # cuty = cy-200
+    # xcut = data[cuty, :]
+    # # x2 = np.exp( - (xstep - cx)**2 / (2 * sx**2))*h + offset
+    # x3 = np.exp( - (xstep - cx)**2 / (2 * sx**2)) * h * (1 + 0.2*np.sin(2*np.pi*xstep*np.cos(th)/36 + 2.5))
+    # ww = wobble(cx, cy, sx, sy, h, th, period, phase, amp, offset)
+    # x2 = ww(xstep, cuty)
+
+    # pl.plot(xstep, xcut, 'k-')
+    # pl.plot(xstep, x2, 'r-')
+    # pl.title(cy)
+
+    # pl.subplot(212)
+    # pl.plot(xstep, (xcut-x2) / xcut)
 
     pl.show()
