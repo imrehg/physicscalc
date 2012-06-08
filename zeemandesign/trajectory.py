51f4b76937625a38a70abdc370934f5dd432adff
Gergely Imreh <imrehg@gmail.com>
2012-06-08 18:58:12 +0800

    trajectory calculations

---
+++ zeemandesign/trajectory.py
@@ -0,0 +1,196 @@
+import numpy as np
+import pylab as pl
+
+import layeroptimize as lo
+import zeemanslower as zs
+import sys
+
+from scipy.integrate import odeint
+import time
+
+def eqmotion(y, t, b):
+    atom, field, s, detu = b
+    B = field([y[0]])[0]
+    dy = y[1]
+    ddy = -zs.hbar*atom.k*atom.G/(2*atom.m) * s / (1 + s + (4 / atom.G**2)*(detu + k * y[1] - zs.uprimehbar*B)**2)
+    return [dy, ddy]
+
+
+def fly(args):
+    print "Starting:", y[1]
+    y0, t, b  = args
+    y = odeint(eqmotion, y0, t, args=(b,))
+    return y
+
+fw, fh = (11.69, 8.27)
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    atom = zs.Rb87()
+
+    sim = np.load(filename)['simulation'][()]
+    wire = sim['wire']
+    setup = sim['setup']
+    eta = sim['eta']
+    v0 = sim['v0']
+    vf = sim['vf']
+
+    looppos, segments, layer = setup['looppos'], setup['segments'], setup['layer']
+
+    print filename
+    eta, v0, vf, detu, R = sim['eta'], sim['v0'], sim['vf'], sim['detu'], sim['R']
+    sl = zs.slowerlength(atom.aslow, eta, v0, vf)
+    z = np.array([0])
+    bfield = zs.bideal(atom, z, eta, v0, vf, detu)
+    z = np.append([-10*R], np.append(np.linspace(0, sl, 61), [sl+10*R]))
+    ze = np.linspace(z[0], z[-1], 201)
+
+    # print setup['csign']
+    # bf = lo.fieldcalc(ze, setup)
+    # # pl.plot(ze, bf)
+    # pl.plot(ze[1:], np.diff(bf))
+
+
+    ratio = 0.25
+    setup['csign'] = [s*ratio if s < 0 else s for s in setup['csign']]
+    # setup['csign'] = [s*ratio if s < 0 else s*ratio for s in setup['csign']]
+
+    # Get the needed current to run the coils:
+    bf = lo.fieldcalc(ze, setup)
+    B0 = zs.bideal(atom, 0, eta, v0, vf, detu)
+    I = B0[0] / lo.fieldcalc(0, setup)[0]
+    print "Current:", I
+    bf *= I
+    
+
+    k = atom.k
+    # # bf = bf*0
+    # # detu = 0
+    # fulldetu = k*v - 2*np.pi*zs.uprimehbar*bf - detu*1e6*2*np.pi
+    # pl.plot(ze, fulldetu/2/np.pi/1e6)
+    # pl.show()
+    
+    # x = (k*v - 2*np.pi*zs.uprimehbar*bf - detu*1e6*2*np.pi)/2/np.pi/1e6
+    # x = bf*1e4
+
+
+    ub = zs.bohrmag/zs.h / 1e4
+    # # x = k*v/2/np.pi/1e6 - bf*1e4 * ub / 1e6
+    # # x = bf*1e4 *ub/1e6 - k*v/2/np.pi/1e6
+    # x = 2*(zs.hbar*k/zs.bohrmag*np.sqrt(v0*v0 - 2*eta*atom.aslow*ze) - 2*np.pi*detu*1e6/zs.uprimehbar)
+    x = zs.bideal(atom, ze, eta, v0, vf, detuning=detu)
+    
+    # # vx = (2*np.pi*zs.uprimehbar*bf/eta + detu*1e6*2*np.pi)/ k
+
+    # y = 1/(-2*np.pi*detu*1e6 + k*v0 - zs.uprimehbar*x)
+
+    filename = "trajectory03"
+    fig = pl.figure(figsize=(fw, fh))
+
+    # videal = (zs.uprimehbar*x+2*np.pi*detu*1e6)/k
+    # pl.plot(ze, videal)
+
+    tmax = 1.5/vf
+    t = np.linspace(0, tmax, 1001)
+    s = 2
+    detuw = -2*np.pi*detu*1e6
+    field = lambda z: lo.fieldcalc(z, setup)*I
+    b = (atom, field, s, detuw)
+
+    vfield = (zs.uprimehbar*field(ze)-detuw)/k
+    pl.plot(ze, vfield, 'k-', linewidth=3, label='0 detuning velocity')
+
+    out = []
+    # for v in np.linspace(v0, vf, 1):
+    vlist = np.linspace(v0, vf, 15)
+    # vlist = [v0/2]
+    z0 = -0.3
+    for v in vlist:
+        print "Velocity: %.1f" %(v)
+        start = time.time()
+        y0 = [-0.3, v]
+        y = odeint(eqmotion, y0, t, args=(b,))
+        # pl.plot(y[:,0], y[:, 1], label="v=%.1f" %(v))
+        pl.plot(y[:,0], y[:, 1])
+        out += [y]
+        print "Elapsed: %.1fs" %(time.time()-start) 
+
+    pl.xlim([z0, ze[-1]])
+    pl.ylim([0, v0+20])
+
+    np.savez(filename, vlist, out)
+    # pl.plot(ze, x)
+    # pl.plot(ze, y)
+    # pl.plot([0], [v0], 'x')
+    pl.xlabel('z position (m)')
+    pl.ylabel('velocity (m/s)');
+
+    # pl.plot(ze, bf)
+    # # pl.plot(ze[1:], np.diff(bf))
+    pl.legend(loc="best")
+
+    pl.savefig("%s.pdf" %(filename))
+    pl.savefig("%s.png" %(filename))
+    pl.show()
+
+
+    # tmax = 1.5/vf
+    # t = np.linspace(0, tmax, 51)
+    # z0 = -0.3
+    # s = 10
+    # detuw = -2*np.pi*detu*1e6
+    # field = lambda z: lo.fieldcalc(z, setup)*I
+    # b = (atom, field, s, detuw)
+
+    # TASKS = [([v, z0],
+    #           t,
+    #           b) for v in range(250, 40, -50)]
+
+
+    # try:
+    #     import multiprocessing as processing
+    # except:
+    #     import processing
+    # NUMBER_OF_PROCESSES = processing.cpu_count()
+    # pool = processing.Pool(processes=NUMBER_OF_PROCESSES)  
+
+    # try:
+    #     out = pool.map_async(fly, TASKS).get(999999)
+    # except KeyboardInterrupt:
+    #     pool.terminate()
+    #     sys.exit(0)
+
+    # print out
+
+
+    # for i in range(len(layer)):
+    #     if np.diff(segments[i]) != 0:
+    #         print "%7.2f mm to %7.2f mm: %3d turns / %3d layers" %((looppos[segments[i][0]]-wire[0]/2)*1000, (looppos[segments[i][1]-1]+wire[0]/2)*1000, np.diff(segments[i]), layer[i])
+    # sta = looppos[segments[1][0]]-wire[0]/2
+    # stb = looppos[segments[-2][1]-1]+wire[0]/2  # The upper boundary is the start of the next layer, need -1
+    # efflen = zs.slowerlength(atom.aslow, eta, v0, vf) * 1000
+    # coillen = (stb - sta)*1000
+    # print "Effective length: %.1f mm" %(efflen)
+    # print "Slower coils' length: %.1f mm (%.1f mm extra)" %(coillen, (coillen-efflen))
+
+    # layers = lo.getLayerNumber(setup)
+
+    # fig = pl.figure(figsize=(fh, fw))
+    # fig.text(0.5, 0.95, '%s: slower = %g m, Max B = %g G' %(filename, sl, max(bfield)*1e4 ), horizontalalignment='center')
+    # pl.subplot(211)
+    # pl.plot(looppos, layers[0], 'k.')
+    # pl.xlim([looppos[0], looppos[-1]])
+    # pl.ylim([0, max(layer)+1])
+    # pl.xlabel('Position (m)')
+    # pl.ylabel('Layer number')
+
+    # pl.subplot(212)
+    # pl.plot(ze, lo.fieldcalc(ze, setup)/lo.fieldcalc(0, setup), 'k-', linewidth=3)
+    # pl.plot(ze, ze*0, 'k--', linewidth=1)
+    # pl.xlim([looppos[0], looppos[-1]])
+    # pl.xlabel('Position (m)')
+    # pl.ylabel('Normalized B field')
+
+    # pl.savefig("%s.pdf" %(filename))
+    # pl.savefig("%s.png" %(filename))
+    # pl.show()
---
+++ zeemandesign/trajectory3.py
@@ -0,0 +1,310 @@
+from __future__ import division
+import numpy as np
+import pylab as pl
+
+import layeroptimize as lo
+import zeemanslower as zs
+import sys
+
+from scipy.integrate import odeint
+from scipy.optimize import fmin
+import time
+
+def eqmotion(y, t, b):
+    a0, s, detu, field = b
+    B = field(y[0])
+    dy = y[1]
+    ddy = a0 * s / (1 + s + 4*(detu + y[1] - B)**2)
+    return [dy, ddy]
+
+def fly(args):
+    y0, t, b  = args
+    # print args
+    # print "Starting:", y[1]
+    y0, t, b  = args
+    y = odeint(eqmotion, y0, t, args=(b,))
+    return y
+
+fieldfile = "16_AWG12Coat_10.npz"
+atom = zs.Rb87()
+
+sim = np.load(fieldfile)['simulation'][()]
+wire = sim['wire']
+setup = sim['setup']
+eta = sim['eta']
+v0 = sim['v0']
+vf = sim['vf']
+looppos, segments, layer = setup['looppos'], setup['segments'], setup['layer']
+
+eta, v0, vf, detu, R = sim['eta'], sim['v0'], sim['vf'], sim['detu'], sim['R']
+sl = zs.slowerlength(atom.aslow, eta, v0, vf)
+z = np.array([0])
+bfield = zs.bideal(atom, z, eta, v0, vf, detu)
+z = np.append([-10*R], np.append(np.linspace(0, sl, 61), [sl+10*R]))
+ze = np.linspace(z[0], z[-1], 201)
+
+ratio = 0.25
+filename = 'trajectory01.npz'
+simed = False
+if simed:
+    setup['csign'] = [s*ratio if s < 0 else s for s in setup['csign']]
+    # Get the needed current to run the coils:
+    bf = lo.fieldcalc(ze, setup)
+    B0 = zs.bideal(atom, 0, eta, v0, vf, detu)
+    I = B0[0] / lo.fieldcalc(0, setup)[0]
+    print "Current:", I
+    bf *= I
+    field = lambda z: lo.fieldcalc(z, setup)*I
+else:
+    bf = zs.bideal(atom, ze, eta, v0, vf, detu)
+    field = lambda z: zs.bideal(atom, z, eta, v0, vf, detu)
+
+k = atom.k
+s = 1
+detuw = -2*np.pi*detu*1e6
+
+# vfield = (zs.uprimehbar*field(ze)-detuw)/k
+
+data = np.load(filename)
+# v = data['arr_0']
+y = data['arr_1']
+n = 0
+z = y[n][:, 0]
+v = y[n][:, 1]
+
+vfield = (zs.uprimehbar*field(z)-detuw)/k
+
+## http://adorio-research.org/wordpress/?p=4493
+from math import *
+def polyCubicRoots(a,b, c):
+    print "input=", a,b,c
+    aby3 = a / 3.0
+    p = b - a*aby3
+    q = (2*aby3**2- b)*(aby3) + c
+    X = (p/3.0)**3
+    Y = (q/2.0)**2
+    Q = X + Y
+    D = 18*a*b*c-4*a**3*c+a**2*b**2-4*b**3-27*c**2
+    print "Q=", Q, D
+    if Q >= 0:
+       sqQ = sqrt(Q)
+       A0 = -q/2.0 + sqQ
+       A = A0**(1/3.0) if A0 > 0 else -(-A0)**(1/3.0)
+       B0 = -q/2.0 - sqQ
+       B = B0**(1/3.0) if B0 > 0 else -(-B0)**(1/3.0)
+       r1 = A + B - aby3
+       re = -(A+B)/2.0-aby3
+       im = sqrt(3.0)/2.0*(A-B)
+       r2 = re+im*1j
+       r3 = re-im*1j
+    else:
+       # This part has been tested.
+       p3by27= sqrt(-p**3/27.0)
+       costheta = -q/2.0/ p3by27
+       print "@@@ costheta=", costheta
+       alpha = acos(costheta)
+       mag = 2 * sqrt(-p/3.0)
+       alphaby3 = alpha/3.0
+       r1 = mag  * cos(alphaby3) - aby3
+       r2 = -mag * cos(alphaby3+ pi/3)-aby3
+       r3 = -mag * cos(alphaby3- pi/3) -aby3
+    return r1, r2, r3
+
+def cubic(B, Bz):
+    x = detuw - zs.uprimehbar * B
+    y = zs.uprimehbar * Bz
+    a = 4*k**2/atom.G**2
+    b = 8*k*x/atom.G**2
+    c = (1 + s + 4*x**2/atom.G**2)
+    d = zs.hbar*k**2*atom.G*s/(2*atom.m*y)
+
+    b /= a
+    c /= a
+    d /= a
+    a = 1
+
+    polyCubicRoots(b, c, d)
+
+    return polyCubicRoots(b, c, d)
+
+zerod = lambda v, B, Bz: zs.hbar * k**2 * atom.G / (v * 2*atom.m)*s/(1+s+4/atom.G**2 * (detuw + k*v - zs.uprimehbar*B)**2) + zs.uprimehbar*Bz
+
+res = []
+vals = []
+nums = range(len(z))
+# nums = [26]
+for num in nums:
+    zx = z[num]
+    print ">>> ", num
+    if zx < 0 or zx > 0.58:
+        res += [np.nan]
+        vals += [0]
+        continue
+    vx = v[num]
+    B = field([zx])[0]
+    vx0 = (zs.uprimehbar*B - detuw)/k
+    vx = vx0
+    zstep = 0.0001
+    Bz = (np.diff(field([zx-zstep/2, zx+zstep/2]))/zstep)[0]
+    # print zx, B, Bz, vx
+
+    # zdd =  zerod(vx, B, Bz)
+    # print zerod(vx*1.01, B, Bz)
+    # fminfunc = lambda v, B, Bz: abs(zerod(v, B, Bz))
+    # vxnew = fmin(fminfunc, vx, args=(B, Bz))
+
+    try:
+        vxpos = cubic(B, Bz)
+    except (RuntimeError):
+        res += [np.nan]
+        continue
+    vxnew = 0
+    for vval in vxpos:
+        # print vxpos, vval
+        if np.imag(vval) != 0:
+            # We have complex roots, that means we jumped the shark
+            vxnew = np.nan
+            break
+        try:
+            if vval < vx0 and vval > vxnew:
+                vxnew = vval
+        except (ValueError):
+            vxnew = np.nan
+            break
+    # vals += [zerod(vxnew, B, Bz)]
+    print "=>>", vxnew
+    res += [vxnew]
+res = np.array(res)
+
+
+dres = (detuw + k*res - zs.uprimehbar*field(z))/(atom.G/2)
+print res
+
+L = 2*np.pi / atom.k
+T = 1 / atom.G
+V = L/T
+A = L/T/T
+v0p = v0 / V
+vfp = vf / V
+# s = 1
+# eta = s / (1+s)
+eta = 0.5
+s = eta / (1-eta)
+print s
+ap = -zs.hbar * atom.k * atom.G / (2 * atom.m) / A
+zx = 0.58 / L
+tp = (vfp - v0p)/ (ap * eta)
+zz = v0p*tp + 0.5 * eta * ap * tp**2 
+print v0p, vfp, ap
+print tp, zx, zz*L, (vfp**2 - v0p**2)/(2 * eta * ap)*L
+
+zl = np.linspace(0, zz, 101)
+# bx = np.sqrt(v0p**2 + 2*ap*eta*zl)
+bx = np.sqrt(v0p**2 + 2*ap*eta*zl)
+# bx = v0p + ap / v0p * zl
+# bx = (bx - v0p) / 2 + v0p
+
+detu = 0
+
+fields = lambda z : np.sqrt(v0p**2 + 2*ap*eta*z)
+
+y0 = [0, v0p]
+tp *= 2
+t = np.linspace(0, tp, 201)
+s = 1
+args = (ap, s, detu, fields)
+y = fly((y0, t, args))
+
+y0 = [0, v0p*0.8]
+t = np.linspace(0, tp, 201)
+s = 1
+args = (ap, s, detu, fields)
+y2 = fly((y0, t, args))
+
+
+# pl.subplot(211)
+# pl.plot(z/L, vfield/V)
+pl.plot(zl, bx, '.-')
+# pl.plot(z/L, v/V)
+# pl.plot(z/L, res/V, 'k-', linewidth=2)
+pl.plot(y[:, 0], y[:, 1], 'r-')
+pl.plot(y2[:, 0], y2[:, 1], 'b-')
+pl.xlim([0, 0.6/L])
+
+# pl.subplot(212)
+# pl.plot(y[:, 0], fields(y[: ,0]) - y[:, 1], 'r.-')
+# pl.xlim([0, 0.6/L])
+
+
+
+# print zerod(vxnew, B, Bz)
+# print cubic(vxnew, B, Bz)
+# pl.figure()
+# vt = np.linspace(0, vxnew*2, 101)
+# pl.plot(vt, cubic(vt, B, Bz))
+
+# pl.figure()
+# pl.plot(z, v)
+# pl.plot(z, vfield)
+# pl.plot(z, res, '--')
+# pl.xlim([0, 0.6])
+
+# pl.figure()
+
+############
+# pl.subplot(212)
+# pl.plot(z, vfield)
+# pl.plot(z, v)
+# pl.plot(z, res, 'k-', linewidth=2)
+# pl.xlim([0, 0.6])
+
+
+# pl.figure()
+# pl.plot(z, vfield)
+# pl.plot(z, v)
+# pl.plot(z, res, 'k-', linewidth=2)
+# pl.xlim([0, 0.6])
+
+
+# pl.subplot(2,2,1)
+# pl.plot(z, vfield)
+# pl.plot(z, v)
+# pl.plot(z, res, 'k-', linewidth=2)
+# pl.xlim([0, 0.6])
+
+# pl.subplot(2,2,2)
+# delta = detuw + k * v - zs.uprimehbar * field(z)
+# pl.plot(z, delta/(atom.G/2))
+# pl.plot(z, dres)
+# pl.xlim([0, 0.6])
+# pl.ylim([-5, 1])
+
+# pl.subplot(2,2,3)
+# dz = np.diff(delta)/np.diff(z)
+# Bz = np.diff(field(z))/np.diff(z)
+# vz = np.diff(v)/np.diff(z)
+
+# pl.plot(z[1:], dz, label='dz')
+# pl.plot(z[1:], -zs.uprimehbar*Bz, label='Bz')
+# pl.plot(z[1:], k*vz, label='vz')
+# pl.plot(z[1:], k*vz-zs.uprimehbar*Bz)
+# # pl.plot(z[1:], zerod(res[1:], field(z[1:]), Bz), 'k--')
+# # pl.plot(z, vals, 'b--')
+# pl.legend(loc='best')
+
+# pl.xlim([0, 0.6])
+
+# pl.subplot(2,2,4)
+# # dv = k*np.diff(delta)/np.diff(vz)
+# # dB = -zs.uprimehbar * np.diff(delta)/np.diff(field(z))
+# dv = np.diff(delta)/np.diff(v)
+# dB = np.diff(delta)/np.diff(field(z))
+# vz = np.diff(v)/np.diff(z)
+# Bz = np.diff(field(z))/np.diff(z)
+# pl.plot(z[1:], dv*vz)
+# pl.plot(z[1:], dB*Bz)
+# pl.xlim([0, 0.6])
+# # lim = 1
+# # pl.ylim([-lim, lim])
+
+pl.show()
