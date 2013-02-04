337091286e5fa4d5cdfd7e7c8886973e03a4aee8
Gergely Imreh <imrehg@gmail.com>
2013-02-04 17:37:13 +0800

    Add more comments to coil calculation

--- helmholtz2/coils.py
+++ helmholtz2/coils.py
@@ -27,16 +27,24 @@ def loop(params, pos, simtitle=""):
     simtitle: Simulation title for plot
     """
     R0, L0, d, n, m = params
+
+    # Calculate the field created by all wires
     field = numpy.zeros(len(pos))
     for i in range(n):
         for j in range(m):
-            rij = R0 + d*(i + 0.5)
-            lij = pos - d*(j + 0.5)
+            rij = R0 + d*(i + 0.5)  # positive offset going out
+            lij = pos - d*(j + 0.5)  # negative offset going away from the centre
             field += bz(rij, lij)
+
+    # Anti-Helmholtz configuration
     field2 = field[::-1]
     totalfield = field-field2
+
+    # Gradient fit
     p = numpy.polyfit(pos-L0, totalfield, 1)
-    gradcm = p[0] / 100
+    gradcm = p[0] / 100  # Gradient G/cm
+
+    # plot
     pl.plot((pos-L0)*100, totalfield, 'ko', label="Calculated")
     pl.plot((pos-L0)*100, numpy.polyval(p, pos-L0), 'r-', label="Linear fit", linewidth=2)
     pl.xlabel("Distance from centre (cm)")
@@ -44,18 +52,21 @@ def loop(params, pos, simtitle=""):
     pl.legend(loc="best")
     pl.title(simtitle+" gradient: %.2f (G/cm)/A" %(gradcm))
     pl.grid()
+
     return gradcm
 
 if __name__ == "__main__":
-    import sys
-    R0 = 55e-3
-    L0 = 29.26e-3
+
+    # General parameters
+    R0 = 55e-3  # inner radius
+    L0 = 29.26e-3  # centre offset
 
     # Build settings
     d = 4e-3
     n, m = 3, 6
     simtitle = "Hollow core"
 
+    # Calculation
     params = (R0, L0, d, n, m)
     pos = numpy.linspace(L0-0.01, L0+0.01, 41)
     gradcm = loop(params, pos, simtitle)
